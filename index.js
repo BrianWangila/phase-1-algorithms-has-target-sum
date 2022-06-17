437316a4d7a5053be585892fa3828e792e178111
Brian Wangila <bwanjalah@gmail.com>
2022-06-17 20:04:01 +0300

    assignment complete

--- index.js
+++ index.js
@@ -1,17 +1,48 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
+
+
+  for (let i of array){
+    let j = target - i;
+    if (array.includes(j)){
+      return true;
+
+    }else{
+      return false
+    }
+      }
+
 }
 
 /* 
   Write the Big O time complexity of your function here
+
+
 */
+//function to ad tow numbers
+
 
 /* 
   Add your pseudocode here
+  1. get an array of numbers as input
+  2. get a second number as input
+  3. Take the first number and add it to the second number
+  4. return the sum of the two numbers
+
+  or
+  1. get an array of numbers as input
+  2. get a second number as input
+  3. take the second number and subtract one number in the array
+  4 if the result is a number that is in the array, then return true
+  5 else, return false
+
+
 */
 
 /*
   Add written explanation of your solution here
+
+
 */
 
 // You can run `node index.js` to view these console logs
